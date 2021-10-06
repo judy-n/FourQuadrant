a663475b9a8e45f72abe48f84fffce71191da1bb
Judy Naamani <judy.naamani@mail.utoronto.ca>
2021-10-06 19:04:58 -0400

    setting up

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get('/:boardID', function(req, res){
+  res.sendFile(path.join(__dirname, '/board.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
