0efd2e275e1e0dec5b8da4e77fa9cdea2f0fcd6d
judy-n <judynaamani@icloud.com>
2021-10-22 12:46:23 -0400

    adding CRUD

--- mongo.js
+++ mongo.js
@@ -9,6 +9,7 @@ async function main(){
         // Connect to the MongoDB cluster
         await client.connect();
         // Make the appropriate DB calls
+        // await listDatabases(client); 
         
     } catch (e) {
         console.error(e);
@@ -41,7 +42,6 @@ class Note {
 async function createBoard(client){
     const newBoard = new Board()
     await client.db("FourQuadrant").collection("Boards").insertOne(newBoard)
-    console.log("created a board!");
     return newBoard;
 }
 
@@ -49,11 +49,48 @@ async function createNote(client, board){
     const newNote = new Note()
     await client.db("FourQuadrant").collection("Notes").insertOne(newNote)
     board.notes.push(newNote)
-    console.log("created a note!");
-    console.log(board.notes)
+    await client.db("FourQuadrant").collection("Boards").updateOne({_id: board._id}, {$set: board})
     return newNote;
 }
 
+async function getNotes(board){
+    return board.notes;
+}
+
+async function removeNote(client, board, note){
+    let j = board.notes.indexOf(note);
+    board.notes.splice(j, 1)
+    let q = {_id: board._id};
+    let newV = {$set: board}
+    await client.db("FourQuadrant").collection("Boards").updateOne(q, newV);
+}
+
+async function readBoard(client, id){
+    const res = await client.db("FourQuadrant").collection("Boards").findOne({_id: id})
+    if (res) {
+        console.log(res)
+    }
+    else {
+        console.log("Couldn't find this Board")
+    }
+}
+
+async function readNote(client, id){
+    const res = await client.db("FourQuadrant").collection("Notes").findOne({_id: id})
+    if (res) {
+        console.log(res)
+    }
+    else {
+        console.log("Couldn't find this Note")
+    }
+}
+
+async function removeBoard(client, board){
+    for (let i = 0; i<board.notes.length ; i++){
+        await client.db("FourQuadrant").collection("Notes").deleteOne({_id: board.notes[i]._id});
+    }
+    await client.db("FourQuadrant").collection("Boards").deleteOne({_id: board._id})
+}
 
 
 
