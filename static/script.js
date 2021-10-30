f7fdc5d4fcedcbd53dc16f66b0cb631c56ad8b55
judy-n <judynaamani@icloud.com>
2021-10-30 18:18:18 -0400

    working on homepage

---
+++ static/script.js
@@ -0,0 +1,14 @@
+var granimInstance = new Granim({
+    element: '#granim-canvas',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
