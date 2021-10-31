928818deff9b1356655533889df7b828b1c6c883
JLambertazzo <jbertazzolambert@gmail.com>
2021-10-31 02:10:37 -0400

    made frontend request functions

---
+++ client/static/js/actions.js
@@ -0,0 +1,48 @@
+// where the frontend communicates with the backend
+const instance = axios.create({
+    baseURL: '/api',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'}
+});
+
+const createBoard = () => {
+    return instance.post('/board')
+        .then(res => res.data.board)
+        .catch(err => console.error)
+}
+
+const createNote = (board_id, quadrant) => {
+    return instance.post(`/note/${board_id}`, quadrant)
+        .then(res => res.data.newNote)
+        .catch(err => console.error)
+}
+
+const getBoard = (board_id) => {
+    return instance.get(`/board/${board_id}`)
+        .then(res => res.data.board)
+        .catch(err => console.error)
+}
+
+const getNote = (note_id) => {
+    return instance.get(`/note/${note_id}`)
+        .then(res => res.data.note)
+        .catch(err => console.error)
+}
+
+const deleteBoard = (board_id) => {
+    return instance.delete(`/board/${board_id}`)
+        .then(res => res.data.board)
+        .catch(err => console.error)
+}
+
+const deleteNote = (note_id) => {
+    return instance.delete(`/note/${note_id}`)
+        .then(res => res.data.note)
+        .catch(err => console.error)
+}
+
+const updateNote = (note) => {
+    return instance.patch('/note', note)
+        .then(res => res.data.note)
+        .catch(err => console.error)
+}
