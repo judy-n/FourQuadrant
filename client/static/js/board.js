87eaeb2a863f8732dea71f2eb3bc8c0bbbf427d4
judy-n <judynaamani@icloud.com>
2021-10-31 20:19:52 -0400

    stickiesgit status

--- client/static/js/board.js
+++ client/static/js/board.js
@@ -1,2 +1,113 @@
+
 const socket = io()
 socket.emit('connected to', window.location.href.split('/')[3])
+
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const stickyArea = document.querySelector(
+    '#stickies-container'
+  );
+
+  const createStickyButton = document.querySelector(
+    '#createsticky'
+  );
+
+  const stickyTitleInput = document.querySelector('#stickytitle');
+  const stickyTextInput = document.querySelector('#stickytext');
+
+  const deleteSticky = e => {
+    e.target.parentNode.remove();
+  };
+
+  let isDragging = false;
+  let dragTarget;
+
+  let lastOffsetX = 0;
+  let lastOffsetY = 0;
+
+  function drag(e) {
+    if (!isDragging) return;
+
+    // console.log(lastOffsetX);
+
+    dragTarget.style.left = e.clientX - lastOffsetX + 'px';
+    dragTarget.style.top = e.clientY - lastOffsetY + 'px';
+  }
+
+  function createSticky() {
+    const newSticky = document.createElement('div');
+    const html = `<h3>${stickyTitleInput.value.replace(
+      /<\/?[^>]+(>|$)/g,
+      ''
+    )}</h3><p>${stickyTextInput.value
+    .replace(/<\/?[^>]+(>|$)/g, '')
+    .replace(
+      /\r\n|\r|\n/g,
+      '<br />'
+    )}</p><span class="deletesticky">&times;</span>`;
+    newSticky.classList.add('drag', 'sticky');
+    newSticky.innerHTML = html;
+    // newSticky.style.backgroundColor = randomColor();
+    stickyArea.append(newSticky);
+    positionSticky(newSticky);
+    applyDeleteListener();
+    clearStickyForm();
+  }
+  function clearStickyForm() {
+    stickyTitleInput.value = '';
+    stickyTextInput.value = '';
+  }
+  function positionSticky(sticky) {
+    sticky.style.left =
+      window.innerWidth / 2 -
+      sticky.clientWidth / 2 +
+      (-100 + Math.round(Math.random() * 50)) +
+      'px';
+    sticky.style.top =
+      window.innerHeight / 2 -
+      sticky.clientHeight / 2 +
+      (-100 + Math.round(Math.random() * 50)) +
+      'px';
+  }
+
+  function editSticky() {}
+
+  function stripHtml(text) {
+    return text.replace(/<\/?[^>]+(>|$)/g, '');
+  }
+
+  function randomColor() {
+    const r = 200 + Math.floor(Math.random() * 56);
+    const g = 200 + Math.floor(Math.random() * 56);
+    const b = 200 + Math.floor(Math.random() * 56);
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+  }
+
+  function applyDeleteListener() {
+    let deleteStickyButtons = document.querySelectorAll(
+      '.deletesticky'
+    );
+    deleteStickyButtons.forEach(dsb => {
+      dsb.removeEventListener('click', deleteSticky, false);
+      dsb.addEventListener('click', deleteSticky);
+    });
+  }
+
+  window.addEventListener('mousedown', e => {
+    if (!e.target.classList.contains('drag')) {
+      return;
+    }
+    dragTarget = e.target;
+    dragTarget.parentNode.append(dragTarget);
+    lastOffsetX = e.offsetX;
+    lastOffsetY = e.offsetY;
+    // console.log(lastOffsetX, lastOffsetY);
+    isDragging = true;
+  });
+  window.addEventListener('mousemove', drag);
+  window.addEventListener('mouseup', () => (isDragging = false));
+
+  createStickyButton.addEventListener('click', createSticky);
+  applyDeleteListener();
+});
+
